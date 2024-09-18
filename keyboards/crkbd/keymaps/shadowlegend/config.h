ccb309dab7e780aba72030668a1258d3c79bc30a
shadowlegend <lyheng0@gmail.com>
2024-09-19 00:35:53 +0700

    add shadowlegend crkbd keyboard

---
+++ keyboards/crkbd/keymaps/shadowlegend/config.h
@@ -0,0 +1,67 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+//#define USE_MATRIX_I2C
+
+//#define QUICK_TAP_TERM 0
+#define PERMISSIVE_HOLD
+#define TAPPING_TERM 180
+
+#define OLED_FADE_OUT
+#define OLED_FADE_OUT_INTERVAL 4
+#define OLED_TIMEOUT 60000
+#define OLED_BRIGHTNESS 128
+#define OLED_DISPLAY_WIDTH 128
+#define OLED_DISPLAY_HEIGHT 32
+
+#define RGB_MATRIX_LED_FLUSH_LIMIT 16
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 255
+
+#define ENABLE_RGB_MATRIX_HUE_WAVE
+#define ENABLE_RGB_MATRIX_BREATHING
+#define ENABLE_RGB_MATRIX_STARLIGHT
+#define ENABLE_RGB_MATRIX_SOLID_SPLASH
+#define ENABLE_RGB_MATRIX_HUE_BREATHING
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+
+#ifdef RGBLIGHT_ENABLE
+    #define RGBLIGHT_EFFECT_BREATHING
+    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+    #define RGBLIGHT_EFFECT_KNIGHT
+    #define RGBLIGHT_EFFECT_CHRISTMAS
+    #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+    #define RGBLIGHT_EFFECT_ALTERNATING
+    #define RGBLIGHT_EFFECT_TWINKLE
+
+    #undef RGBLIGHT_EFFECT_SNAKE
+    #undef RGBLIGHT_EFFECT_RGB_TEST
+    #undef RGBLIGHT_EFFECT_RAINBOW_SWIRL
+
+    #define RGBLIGHT_SPLIT
+
+    #define RGBLIGHT_LIMIT_VAL 255
+    #define RGBLIGHT_HUE_STEP 16
+    #define RGBLIGHT_SAT_STEP 16
+    #define RGBLIGHT_VAL_STEP 16
+    #define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_MOOD
+#endif
+
---
+++ keyboards/crkbd/keymaps/shadowlegend/rules.mk
@@ -0,0 +1,12 @@
+
+VIA_ENABLE = yes
+CONVERT_TO = helios
+
+LTO_ENABLE = yes
+CONSOLE_ENABLE = no
+
+WPM_ENABLE = yes
+OLED_ENABLE = yes
+MOUSEKEY_ENABLE = no
+RGBLIGHT_ENABLE = yes
+
